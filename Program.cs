c38d6be498433845a6f647b4d2c188b0bd2a8925
caioaugustofm <caioaugusto.fm@gmail.com.br>
2026-05-06 10:16:33 -0300

    Desafio

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+class Program
+{
+    struct Personagem
+    {
+        public string Nome;
+        public int Forca;
+        public int Agilidade;
+        public int Inteligencia;
+        public int Vida;
+    }
+
+    static void Main()
+    {
+        Personagem p1 = CriarPersonagem("Homem-Aranha", 7, 9, 10, 7);
+        Personagem p2 = CriarPersonagem("Venom", 8, 7, 7, 9);
+        ExibirPersonagem(p1);
+        ExibirPersonagem(p2);
+        Console.WriteLine("Batalha");
+        int pontosP1 = 0;
+        int pontosP2 = 0;
+        if (CalcularOfensiva(p1) > CalcularOfensiva(p2))
+        {
+            Console.WriteLine("Ofensiva: " + p1.Nome + " venceu!");
+            pontosP1++;
+        }
+        else
+        {
+            Console.WriteLine("Ofensiva: " + p2.Nome + " venceu!");
+            pontosP2++;
+        }
+
+        if (CalcularEstrategia(p1) > CalcularEstrategia(p2))
+        {
+            Console.WriteLine("Estratégia: " + p1.Nome + " venceu!");
+            pontosP1++;
+        }
+        else
+        {
+            Console.WriteLine("Estratégia: " + p2.Nome + " venceu!");
+            pontosP2++;
+        }
+        if (CalcularResistencia(p1) > CalcularResistencia(p2))
+        {
+            Console.WriteLine("Resistência: " + p1.Nome + " venceu!");
+            pontosP1++;
+        }
+        else
+        {
+            Console.WriteLine("Resistência: " + p2.Nome + " venceu!");
+            pontosP2++;
+        }
+
+        Console.WriteLine("Resultado");
+
+        if (pontosP1 > pontosP2)
+            Console.WriteLine(p1.Nome + " venceu a batalha!");
+        else if (pontosP2 > pontosP1)
+            Console.WriteLine(p2.Nome + " venceu a batalha!");
+        else
+            Console.WriteLine("Empate");
+    }
+
+    static Personagem CriarPersonagem(string nome, int f, int a, int i, int v)
+    {
+        Personagem p;
+        p.Nome = nome;
+        p.Forca = f;
+        p.Agilidade = a;
+        p.Inteligencia = i;
+        p.Vida = v;
+        return p;
+    }
+
+    static void ExibirPersonagem(Personagem p)
+    {
+        Console.WriteLine("\nPersonagem: " + p.Nome);
+        Console.WriteLine("Força: " + p.Forca);
+        Console.WriteLine("Agilidade: " + p.Agilidade);
+        Console.WriteLine("Inteligência: " + p.Inteligencia);
+        Console.WriteLine("Vida: " + p.Vida);
+    }
+
+    static int CalcularOfensiva(Personagem p)
+    {
+        return p.Forca + p.Agilidade;
+    }
+
+    static int CalcularEstrategia(Personagem p)
+    {
+        return p.Inteligencia * 2;
+    }
+
+    static int CalcularResistencia(Personagem p)
+    {
+        return p.Vida + p.Agilidade;
+    }
+}
